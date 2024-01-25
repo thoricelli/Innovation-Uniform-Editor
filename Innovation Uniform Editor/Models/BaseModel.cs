51743a2d6eb94f67bbc97fe10e5c9962028200e1
thoricelli <roanheylen@hotmail.com>
2024-01-25 22:56:15 +0100

    Adding base repository for models.

---
+++ Innovation Uniform Editor/Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovation_Uniform_Editor.Models
+{
+    public class BaseModel
+    {
+        private int _id;
+        public int Id { get { return _id; } }
+
+        private Guid _guid;
+        public Guid Guid { get { return _guid; } }
+    }
+}
---
+++ Innovation Uniform Editor/Repositories/BaseRepository.cs
@@ -0,0 +1,40 @@
+﻿using Innovation_Uniform_Editor.Interfaces;
+using Innovation_Uniform_Editor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovation_Uniform_Editor.Repositories
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : BaseModel
+    {
+        private List<T> _objects;
+        public List<T> Objects { get { return _objects; } }
+        public void AddObject(T obj)
+        {
+            _objects.Add(obj);
+        }
+
+        public void DeleteObject(Guid guid)
+        {
+            _objects.Remove(this.GetObject(guid));
+        }
+
+        public T FindFromId(int id)
+        {
+            return _objects.Find(b => b.Id == id);
+        }
+
+        public T GetObject(Guid guid)
+        {
+            return _objects.Find(b => b.Guid == guid);
+        }
+
+        public T Update(Guid guid, T newObj)
+        {
+            return _objects[_objects.IndexOf(this.GetObject(guid))] = newObj;
+        }
+    }
+}
